ff0c63458fad02b688eb8a0ee69ec41774fe275c
MaxKlammer <maximilian.klammer@betssongroup.com>
2021-03-19 09:33:57 +0100

    added changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <div
+        style={{
+          position: "fixed",
+          top: "60%",
+          bottom: 0,
+          right: 0,
+          left: 0,
+          background: "hsl(195deg, 20%, 86%)",
+        }}
+      ></div>
       <CharacterEditor />
       <Footer />
     </>
